ce31c1eaeca6802e3ffafc34a4b8cc99a9b5abab
Gurmannat12 <73995179+Gurmannat12@users.noreply.github.com>
2021-07-27 20:49:45 +0530

    Update SpaceCraft.js

--- screens/SpaceCraft.js
+++ screens/SpaceCraft.js
@@ -1,19 +1,93 @@
-import React, {Component} from 'react';
-import { Text, 
-         View,  
-         } from 'react-native';
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    Platform,
+    StatusBar,
+    Alert,
+    FlatList,
+    Image,
+    ImageBackground,
+} from "react-native";
+import axios from "axios";
 
-         export default class SpaceCraftScreen extends Component {
-             render(){
-                 return(
-                     <View
-                         style = {{
-                             flex:1,
-                             justifyContent:"center",
-                             alignItems:"center",
-                         }}>
-                             <Text>Space Craft</Text>
-                     </View>
-                 )
-             }
-            }
+export default class SpaceCraft extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            aircrafts: {},
+        };
+    }
+
+    componentDidMount() {
+        this.getData()
+    }
+
+    getData = () => {
+        axios
+            .get("https://ll.thespacedevs.com/2.0.0/config/spacecraft/")
+            .then(response => {
+                this.setState({ aircrafts: response.data.results })
+                console.log(response.data.results)
+            })
+            .catch(error => {
+                Alert.alert(error.message)
+            })
+    }
+
+    renderItem = ({ item }) => {
+        return (
+            <View style={{borderWidth:1,justifyContent:'center',alignItems:'center',marginBottom:10,elevation:10}}>
+                <Image 
+                source={{uri: item.agency.image_url}} style={{width: "100%", height:200, marginTop:15, marginBottom:15, marginRight:10 }}></Image>
+                        
+                <Text style={{fontWeight:'bold', fontSize:20}}>{item.name}</Text>
+                <Text style={{color:'#696969'}}>{item.agency.name}</Text>
+                <Text>DESCRIPTION</Text>
+                <Text style={{color:'#A9A9A9', marginTop: 10, marginLeft: 10 }}>{item.agency.description}</Text>      
+            </View>
+        );
+    };
+
+    keyExtractor = (item, index) => index.toString();
+
+    render() {
+        if (Object.keys(this.state.aircrafts).length === 0) {
+            return (
+                <View
+                    style={{
+                        flex: 1,
+                        justifyContent: "center",
+                        alignItems: "center"
+                    }}>
+                    <Text>Loading</Text>
+                </View>
+            )
+        } else {
+            return (
+                <View 
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}>
+                    <ImageBackground source={require('../assets/stars.gif')} style={{flex: 1, resizeMode: 'cover'}}>
+                <View styles={{flex:0.25}}>
+                    <Text>Space Crafts</Text>
+                </View>
+                <View styles={{flex:0.75}}>
+                    <FlatList
+                        keyExtractor={this.keyExtractor}
+                        data={this.state.aircrafts}
+                        renderItem={this.renderItem}
+                    />
+                    </View>
+                    </ImageBackground>
+                </View >
+            );
+        }
+
+    }
+}
